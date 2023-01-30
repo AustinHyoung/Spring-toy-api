ca96eced94805236050b9f154d0d6cfe397965e8
hakyoung <jhy2297@naver.com>
2023-01-30 23:35:10 +0900

    log4j2 버전 에러 복구, yml datasource hikari, DatabaseConfig

--- toy/src/main/java/com/toy/api/ToyApplication.java
+++ toy/src/main/java/com/toy/api/ToyApplication.java
@@ -1,9 +1,11 @@
 package com.toy.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.toy.api")
 public class ToyApplication {
 
 	public static void main(String[] args) {
