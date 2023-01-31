6ea0e8526ef0298593f976d962bf8ac19a9770ea
hakyoung <jhy2297@naver.com>
2023-01-31 23:44:12 +0900

    mapperscan 변경

--- toy/src/main/java/com/toy/api/ToyApplication.java
+++ toy/src/main/java/com/toy/api/ToyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.toy.api")
 public class ToyApplication {
 
 	public static void main(String[] args) {
